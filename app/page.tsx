dd11772c6fb5baf77b1d29347cb6d1457b741f0f
Ilaha <ferzeliyevailahe89@gmail.com>
2026-08-11 15:15:33 +0400

    style: change text color

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="space-y-4">
           <button
             onClick={() => handleSelectCategory("math")}
-            className="w-full py-4 bg-yellow-500 hover:bg-yellow-600 text-slate-950 font-semibold rounded-xl transition duration-200 shadow-lg cursor-pointer"
+            className="w-full py-4 bg-yellow-500 hover:bg-yellow-600 text-white font-semibold rounded-xl transition duration-200 shadow-lg cursor-pointer"
           >
             Riyaziyyat
           </button>
